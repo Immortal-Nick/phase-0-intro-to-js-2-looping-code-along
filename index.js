587f480baaf6c3e3092807290735d8f82a7330b2
Nixon <kiplangatnixon28@gmail.com>
2024-07-07 19:27:09 +0300

    creates loops

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names) {
+    const thankYouMessages = [];
+
+    for (let i = 0; i < names.length; i++) {
+        thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+
+    return thankYouMessages;
+}
+
+//Function that takes a positive integer and counts down to zero
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
